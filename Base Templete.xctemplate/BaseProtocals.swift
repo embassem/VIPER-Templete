4dab4cb791243ce51e02a37dbb532ede463c11c0
Bassem Abbas <bassem.abbas@ibtikar.net.sa>
2018-11-13 09:22:48 +0200

    add base File

---
+++ Base Templete.xctemplate/BaseProtocals.swift
@@ -0,0 +1,23 @@
+//
+//  Viper.swift
+//  
+//
+//  Created by Bassem Abbas on 6/13/18.
+//
+
+import Foundation
+
+protocol PresenterLifeCycleProtocol {
+  
+    optional func loadView
+    optional func viewDidLoad
+    optional func viewWillAppear
+    optional func viewDidAppear
+    optional func didReceiveMemoryWarning
+    optional func viewWillDisappear
+    optional func viewDidDisappear
+}
+
+protocol BasePresenterProtocol {
+    
+}
