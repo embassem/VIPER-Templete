c74c127b7645759154fe0f01b73930460f2fb0c0
Bassem Abbas <em.bassem@gmail.com>
2019-02-10 00:32:40 +0200

    loadViewControllerFromXib

--- Base Templete.xctemplate/BaseViewController.swift
+++ Base Templete.xctemplate/BaseViewController.swift
@@ -18,4 +18,14 @@ class BaseViewController: UIViewController {
     
     // MARK: - IBOutlets
     
+    /// use this if you add a ViewControllerScene to the Nib not a View
+    static func loadViewControllerFromXib<Controller: BaseViewController>() -> Controller {
+        
+        let nib = UINib(nibName: String(describing: self), bundle: Bundle.main)
+        guard let controller =  nib.instantiate(withOwner: nil, options: nil).first as? Controller else {
+            fatalError("failed to parse top level object in nib to \(String(describing: Controller.self))")
+        }
+        return controller
+    }
 }
+
